fa0df4016f7df4307f0c2dac4dd808a4024631a0
04Sahil <147229728+04Sahil@users.noreply.github.com>
2024-08-22 00:50:43 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('mousemove', (event) =>{
-   const element = document.getElementById('animateMe');
-   const x = event.clientX;
-   const y = event.clientY;
-   element.style.left = '${x}px';
-   element.style.right = '${y}px'; 
-});
